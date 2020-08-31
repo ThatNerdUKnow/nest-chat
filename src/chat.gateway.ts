6cd93334bdb0c4b11e3b55e530a8de5bcf942199
withenex <brandonpi265@gmail.com>
2020-08-31 12:43:27 -0500

    Messages show on client

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,10 +1,38 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { SubscribeMessage, WebSocketGateway, MessageBody, ConnectedSocket } from '@nestjs/websockets';
+import { Socket } from 'dgram';
 
 @WebSocketGateway()
 export class ChatGateway {
+
+clients = [];
+
   @SubscribeMessage('Message')
-  handleMessage(client: any, payload: any): string {
+  handleMessage(@MessageBody() payload: string,@ConnectedSocket() client:Socket) {
     console.log(payload);
-    return 'Hello world!';
+    
+    this.clients.forEach(socket=>{
+      socket.emit("Message",payload);
+    })
+  }
+
+
+  handleConnection(@ConnectedSocket() client:Socket)
+  {
+    console.log("Client Connected")
+    this.clients.push(client);
+    this.clients.forEach(socket =>{
+      socket.emit("Connect","A client has connected")
+    })
+  }
+
+
+  handleDisconnect(@ConnectedSocket()client:Socket)
+  {
+    console.log("Client Disconnected");
+    this.clients.forEach(socket=>{
+      socket.emit("Disconnect","A client has disconnected")
+    })
+    //this.clients.findIndex(client);
+
   }
 }
